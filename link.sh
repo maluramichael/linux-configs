83d266fbf19c3f49ab21433cd3d00294c210607d
Michael Malura <github@malura.me>
2018-07-17 20:49:46 +0200

    Fix link script

--- link.sh
+++ link.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
 currentDirectory=$(pwd)
-cd $HOME
 
-mkdir -p .oh-my-zsh/themes
+mkdir -p "$HOME/.oh-my-zsh/themes"
 
-ln -sf "$currentDirectory/.shell" .shell
-ln -sf "$currentDirectory/.zshrc" .zshrc
-ln -sf "$currentDirectory/.npmrc" .npmrc
-ln -sf "$currentDirectory/.gitconfig" .gitconfig
-ln -sf "$currentDirectory/.oh-my-zsh/themes/devnetik.zsh-theme" ".oh-my-zsh/themes/devnetik.zsh-theme"
-ln -sf "$currentDirectory/.conkyrc" .conkyrc
-ln -sf "$currentDirectory/.conky.d" .conky.d
+ln -s "$currentDirectory/.shell" "$HOME/.shell"
+ln -s "$currentDirectory/.zshrc" "$HOME/.zshrc"
+ln -s "$currentDirectory/.npmrc" "$HOME/.npmrc"
+ln -s "$currentDirectory/.gitconfig" "$HOME/.gitconfig"
+ln -s "$currentDirectory/.oh-my-zsh/themes/devnetik.zsh-theme" "$HOME/.oh-my-zsh/themes/devnetik.zsh-theme"
+ln -s "$currentDirectory/.conkyrc" "$HOME/.conkyrc"
+ln -s "$currentDirectory/.conky.d" "$HOME/.conky.d"
