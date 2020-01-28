9202a75b4f677f8c83c6cb269ee7c600268bb694
Michael Malura <michael@malura.de>
2020-01-28 10:03:28 +0100

    Add more libs

--- bootstrap/clone.libs.sh
+++ bootstrap/clone.libs.sh
@@ -10,10 +10,15 @@ hg clone https://hg.libsdl.org/SDL_mixer
 hg clone https://hg.libsdl.org/SDL_net
 
 # QT
-git clone git://code.qt.io/qt/qt5.git --branch 5.12 qt
+git clone --recurse-submodules git://code.qt.io/qt/qt5.git --branch 5.12 qt
 
 # BOOST
 git clone --recurse-submodules git@github.com:boostorg/boost.git
 
 # OpenCV
 git clone git@github.com:opencv/opencv.git
+
+# Other
+git clone git@github.com:ArashPartow/exprtk.git
+git clone git@github.com:amrayn/easyloggingpp.git
+git clone git@github.com:google/googletest.git
