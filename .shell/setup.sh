a092038faa924502c47dff83ab77b63e55f06204
Michael Malura <michael@malura.de>
2020-02-22 09:58:18 +0100

    Enable tmux by default

--- .shell/setup.sh
+++ .shell/setup.sh
@@ -1,3 +1,7 @@
+if command -v tmux &> /dev/null && [ -n "$PS1" ] && [[ ! "$TERM" =~ screen ]] && [[ ! "$TERM" =~ tmux ]] && [ -z "$TMUX" ]; then
+  exec tmux
+fi
+
 #eval "$(rbenv init -)"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
