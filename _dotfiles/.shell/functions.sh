4aabeeae6bfa747bbe6527aad0d387000f2c450f
Michael Malura <michael@malura.de>
2020-03-03 23:49:28 +0100

    Fix lint errors

--- _dotfiles/.shell/functions.sh
+++ _dotfiles/.shell/functions.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-ips () { ip addr | grep "inet\b" | awk '{print $2}' | cut -d/ -f1 }
-mcd () { [ ! -d "$1" ] && mkdir -p "$1" && cd "$1"; }
-mans () { man $1 | grep -iC2 --color=always $2 | less }
+ips () {
+  ip addr | grep "inet\\b" | awk '{print $2}' | cut -d/ -f1
+}
+
+mcd () {
+  [ ! -d "$1" ] && mkdir -p "$1" && cd "$1" || exit
+}
+
+mans () {
+  man "$1" | grep -iC2 --color=always "$2" | less
+}
+
 dev () {
-  [ ! -d ${DEV_HOME} ] && mkdir -p ${DEV_HOME}
-  cd ${DEV_HOME}
+  [ ! -d "$DEV_HOME" ] && mkdir -p "$DEV_HOME"
+  cd "$DEV_HOME" || exit
   ls
 }
--- _dotfiles/.shell/setup.sh
+++ _dotfiles/.shell/setup.sh
@@ -1,8 +1,11 @@
+#!/usr/bin/env bash
+
 # if command -v tmux &> /dev/null && [ -n "$PS1" ] && [[ ! "$TERM" =~ screen ]] && [[ ! "$TERM" =~ tmux ]] && [ -z "$TMUX" ]; then
 #   exec tmux
 # fi
 
 #eval "$(rbenv init -)"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-source $HOME/.local/bin/virtualenvwrapper.sh
+source "$HOME/.local/bin/virtualenvwrapper.sh"
