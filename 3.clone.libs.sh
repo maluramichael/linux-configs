69abf7d60c485f359e6fa39602272f33b41f1f96
Michael Malura <michael@malura.de>
2020-03-03 23:49:12 +0100

    Refactor compilation of libs and tools

--- 3.clone.libs.sh
+++ 3.clone.libs.sh
@@ -1,42 +1,94 @@
 #!/usr/bin/env bash
+
 set -o pipefail
 set -o nounset
 
-cd $HOME/development/libs
+cd "$HOME/development/libs" || return
 
-build_cmake(){
-    build_dir="$1/build"
-    mkdir -p ${build_dir}
-    cmake -B${build_dir} -H${1}
-    (cd ${build_dir} && make && sudo make install)
+CMAKE_BUILD_TYPE="Release"
+
+build_cmake() {
+  build_dir="$1/build"
+  mkdir -p "$build_dir"
+  cmake _D -B"$build_dir" -H"$1"
+  (cd "$build_dir" && make && sudo make install)
+}
+
+create_build_dir() {
+  build_dir="$1/build"
+  mkdir -p "$build_dir"
+  cd "$build_dir" || return
+}
+
+build_sdl_lib() {
+  ./configure --prefix"$INSTALL_PREFIX"
+  make install
 }
 
 # SDL
 hg clone https://hg.libsdl.org/SDL
-build_cmake SDL
+(
+  create_build_dir SDL
+  cmake -DCMAKE_BUILD_TYPE="$CMAKE_BUILD_TYPE" -DCMAKE_INSTALL_PREFIX="$INSTALL_PREFIX" ..
+)
 
 hg clone https://hg.libsdl.org/SDL_ttf
-build_cmake SDL_ttf
+(
+  create_build_dir SDL_ttf
+  build_sdl_lib
+)
 
 hg clone https://hg.libsdl.org/SDL_image
-build_cmake SDL_image
+(
+  create_build_dir SDL_image
+  build_sdl_lib
+)
 
 hg clone https://hg.libsdl.org/SDL_mixer
-build_cmake SDL_mixer
-
 hg clone https://hg.libsdl.org/SDL_net
-build_cmake SDL_net
 
 # QT
-git clone --recurse-submodules git://code.qt.io/qt/qt5.git --branch 5.12 qt
+mkdir -p qt/source
+mkdir -p qt/build
+(
+  cd qt/source || return
+  git clone --recurse-submodules git://code.qt.io/qt/qt5.git --branch 5.12 .
+  cd ../build || return
+  ../source/configure -opensource -nomake examples -nomake tests -prefix "$INSTALL_PREFIX/qt5"
+  make install
+)
 
 # BOOST
 git clone --recurse-submodules git@github.com:boostorg/boost.git
+(
+  cd boost || return
+  ./bootstrap.sh --prefix="$INSTALL_PREFIX"
+  ./b2 -q -d0 install
+)
 
 # OpenCV
 git clone git@github.com:opencv/opencv.git
+(
+  cd opencv || return
+  mkdir build
+  cd build || return
+  cmake -DCMAKE_BUILD_TYPE="$CMAKE_BUILD_TYPE" -DCMAKE_INSTALL_PREFIX="$INSTALL_PREFIX" ..
+)
 
 # Other
 git clone git@github.com:ArashPartow/exprtk.git
 git clone git@github.com:amrayn/easyloggingpp.git
 git clone git@github.com:google/googletest.git
+
+git clone https://github.com/llvm/llvm-project.git
+(
+  cd llvm-project || return
+  mkdir build
+  cd build || return
+  cmake \
+    -DCMAKE_BUILD_TYPE="$CMAKE_BUILD_TYPE" \
+    -DCMAKE_INSTALL_PREFIX="$INSTALL_PREFIX" \
+    -DLLVM_ENABLE_PROJECTS=clang \
+    ../llvm
+  make install
+)
--- 4.clone.tools.sh
+++ 4.clone.tools.sh
@@ -2,17 +2,40 @@
 set -o pipefail
 set -o nounset
 
-cd $HOME/tools
+cd "$HOME/tools" || return
 
-build_cmake(){
-    build_dir="$1/build"
-    mkdir -p ${build_dir}
-    cmake -B${build_dir} -H${1}
-    (cd ${build_dir} && make)
+build_cmake() {
+  build_dir="$1/build"
+  mkdir -p "$build_dir"
+  cmake -B"$build_dir" -H"$1"
+  (cd "$build_dir" && make)
 }
 
 git clone git@gitlab.kitware.com:cmake/cmake.git
-(cd cmake && ./boostrap --parallel=8)
+(
+  cd cmake || return
+  ./bootstrap --prefix="$INSTALL_PREFIX" --parallel=8
+  ./configure
+  make
+  make install
+)
+
+git clone https://git.savannah.gnu.org/git/make.git
+(
+  cd make || return
+  ./bootstrap --prefix="$INSTALL_PREFIX"
+  ./configure
+  make
+  make install
+)
+
+git clone git://sourceware.org/git/binutils-gdb.git
+(
+  cd binutils-gdb || return
+  ./configure --prefix="$INSTALL_PREFIX"
+  make
+  make install
+)
 
 git clone git@github.com:alacritty/alacritty.git
 (cd alacritty && make binary)
@@ -21,4 +44,7 @@ git clone git@github.com:debauchee/barrier.git
 build_cmake barrier
 
 git clone git@github.com:godotengine/godot.git
-(cd godot && scons platform=x11 target=debug bits=64 )
+(cd godot && scons platform=x11 target=debug bits=64)
+
+git clone https://github.com/themix-project/oomox.git --recursive
+(cd oomox && make)
