7e75845f3f2759ac905a5da05eedfa02013f4fb9
Michael Malura <michael@malura.de>
2020-03-04 16:54:05 +0100

    Update list of installed packages

--- 2.install.packages.sh
+++ 2.install.packages.sh
@@ -3,58 +3,82 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-cd $HOME
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)" # script directory
 
-sudo apt install linux-headers-$(uname -r)
-sudo apt install $(cat apt.packages.list)
+install_packages() {
+  PACKAGES=$(cat ${__dir}/apt.packages.list | awk '{ print $2 }')
+  echo "$PACKAGES" | xargs sudo apt-get install -y
+  sudo apt install "linux-headers-$(uname -r)"
+}
 
-# ZSH
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh) --unattended"
+add_keys() {
+  # Yarn
+  curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+  # VSCode
+  curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >packages.microsoft.gpg
+  sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
+  # Scala
+  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
+}
 
-# Ohy my zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+add_repos() {
+  # Yarn
+  echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+  # VSCode
+  sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+  # Scala
+  echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+}
 
-# ZSH Plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions
+install_zsh() {
+  # ZSH
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh) --unattended"
 
-# NVM
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
-nvm install stable
-nvm use stable
-(cd "$NVM_DIR" && git pull origin)
+  # Ohy my zsh
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-# Rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+  # ZSH Plugins
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+  git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+  git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions
+}
 
-# Yarn
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt update -y; sudo apt install yarn -y
+install_nvm() {
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
+  nvm install stable
+  nvm use stable
+  (cd "$NVM_DIR" && git pull origin)
+}
 
-# VSCode
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
-sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo apt update -y && sudo apt install code
+install_rust() {
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+}
 
-# Python
-# thanks to https://linuxconfig.org/how-to-change-default-python-version-on-debian-9-stretch-linux
-sudo update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1
-sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.7 2
+install_python() {
+  # thanks to https://linuxconfig.org/how-to-change-default-python-version-on-debian-9-stretch-linux
+  sudo update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1
+  sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.7 2
 
-# PIP
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python get-pip.py --user
-pip install --upgrade pip
-pip install virtualenv --user
-pip install http-prompt --user
+  curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+  python get-pip.py --user
+  pip install --upgrade pip
+  pip install virtualenv --user
+  pip install http-prompt --user
+}
 
-# Tmux
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+install_tmux() {
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+}
 
-# Ruby
-#rbenv install 2.3.0
-#rbenv global 2.3.0
+install_packages
+add_keys
+add_repos
+
+sudo apt-get update -y
+
+install_zsh
+install_nvm
+install_rust
+install_python
+install_tmux
