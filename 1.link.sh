1a61425d419111d6f631b9a51db954ec5b01f1ef
Michael Malura <michael@malura.de>
2020-03-23 22:35:53 +0100

    Update scripts and configs

--- 1.link.sh
+++ 1.link.sh
@@ -6,33 +6,32 @@ set -o nounset
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)" # script directory
 __dotfiles="_dotfiles/"
 
-link(){
-    from="${__dir%/}/${1}"
-    to="${HOME%/}/${1#${__dotfiles}}"
-    if [[ ${1} =~ "/" ]]; then
-        directory_structure=$(dirname "$to")
+link() {
+  from="${__dir%/}/${1}"
+  to="${HOME%/}/${1#${__dotfiles}}"
+  if [[ ${1} =~ "/" ]]; then
+    directory_structure=$(dirname "$to")
 
-        if [[ ! -d $directory_structure ]]; then
-            echo "$directory_structure does not exist"
-            mkdir -p "$directory_structure"
-        fi
+    if [[ ! -d $directory_structure ]]; then
+      echo "$directory_structure does not exist"
+      mkdir -p "$directory_structure"
     fi
+  fi
 
-    if [[ -h "$to" ]]; then
-        rm -r "$to"
-    fi
+  if [[ -L "$to" ]]; then
+    rm -r "$to"
+  fi
 
-    if [ -f "$to" ] || [ -d "$to" ]; then
-        echo "Backup existing $to $to.bak"
-        mv "$to" "$to.bak"
-    fi
+  if [ -f "$to" ] || [ -d "$to" ]; then
+    echo "Backup existing $to $to.bak"
+    mv "$to" "$to.bak"
+  fi
 
-    echo "[+] Link $from -> $to"
-    ln -s "$from" "$to"
+  ln -s "$from" "$to"
 }
 
 (
-    echo "Run link script from $__dir"
-    cd "$__dir"
-    find "$__dotfiles" -type f | while read -r file; do link "${file}"; done
+  echo "Run link script from $__dir"
+  cd "$__dir"
+  find "$__dotfiles" -type f | while read -r file; do link "${file}"; done
 )
